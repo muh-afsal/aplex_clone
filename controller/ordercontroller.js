23307ec12fa78e5c6345e01a1c1f44b566d3f279
Muhammed Afsal <afsalhdy@gmail.com>
2024-02-02 01:01:28 +0530

    invoide download bug fix

--- controller/ordercontroller.js
+++ controller/ordercontroller.js
@@ -169,7 +169,7 @@ const GenerateInvoices = async (req, res) => {
 const downloadInvoice = async (req, res) => {
   try {
     const id = req.params.orderId;
-    const filePath = `D:\\brocamp weekly\\Week11-project-1st\\Aplex\\public\\pdf\\${id}.pdf`;
+    const filePath = `public\\pdf\\${id}.pdf`;
     res.download(filePath, `invoice.pdf`);
   } catch (error) {
     console.error("Error in downloading the invoice:", error);
